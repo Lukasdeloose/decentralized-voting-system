e606bf347ea3d0ef793ec03b93a86e15693e1f77
Lukas De Loose <lukas.deloose@ugent.be>
2020-01-27 16:32:42 +0100

    updated blockchain structs

--- project/blockchain/types.go
+++ project/blockchain/types.go
@@ -6,39 +6,60 @@ import (
 )
 
 var Blockchain []Block
+type BlockAck PrivateMessage
+
+type Poll struct {
+	Origin   string
+	Question string
+	Votes    []EncryptedVote
+	Voters	 []string // Hashes of Sciper numbers of people who are allowed to vote
+}
+
+type EncryptedVote struct {
+	Origin string
+	PollID uint32
+	Vote   paillier.Cypher
+}
 
 // New votes cast
 type VoteTx struct {
+	ID   uint32
+	Vote EncryptedVote
+}
 
+// New poll added, finder of block assigns the unique pollID
+type PollTx struct {
+	Poll Poll
+	ID   uint32
+}
+
+
+type SciperTx struct {
+	// TODO Thomas
 }
 
 // New users registered
 type RegisterTx struct {
-
+	// TODO Thomas
 }
-
-// New poll added
-type PollTx struct {
-
-}
-
 
 // Transactions that happened since last Block
 type Transactions struct {
-	 votes []VoteTx
-	 registers []RegisterTx
-	 polls []PollTx
+	Votes     []VoteTx
+	Polls     []PollTx
+	Registers []RegisterTx
+	Scipers[] []SciperTx
 }
 
 type Block struct {
 	Index          int
 	Timestamp      string
-	Scipers        []uint32 // Sciper numbers of all registered citizens, generally 6 digits. Should only be added by the 'government'
+	Transactions   Transactions
 	PaillierPublic paillier.PublicKey
-	Hash           string
-	PrevHash       string
 	Difficulty     int
 	Nonce          string
+	PrevHash       string
+	Hash           string
 }
 
 type BlockMessage struct {
@@ -49,4 +70,3 @@ type BlockMessage struct {
 	VectorClock *StatusPacket
 }
 
-type BlockAck PrivateMessage
